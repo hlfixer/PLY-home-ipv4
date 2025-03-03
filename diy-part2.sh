5efd04ae1bf4c3f4c2446ec9fe4a07b157da8099
hlfixer <33725595+hlfixer@users.noreply.github.com>
2025-03-03 09:58:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,19 +17,3 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/PLY/g' package/base-files/files/bin/config_generate
-rm -rf ./feeds/luci/applications/luci-app-mosdns
-rm -rf ./feeds/luci/applications/luci-app-smartdns
-rm -rf ./feeds/luci/applications/luci-app-v2raya
-rm -rf ./feeds/small/brook
-rm -rf ./feeds/small/trojan-go
-rm -rf ./feeds/small/luci-app-mosdns
-rm -rf ./feeds/small/luci-app-mihomo
-rm -rf ./feeds/small/mihomo
-rm -rf ./feeds/small/luci-app-fchomo
-rm -rf ./feeds/small/luci-app-bypass
-rm -rf ./feeds/small/luci-app-homeproxy
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns,shadowsocks*}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
